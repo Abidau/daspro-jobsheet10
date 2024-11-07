493dc8a57b003e21485befed3403cdb4b10524d1
NaufalAbidAurizky <naufalabidau@gmail.com>
2024-11-07 08:06:29 +0700

    Percobaan2

---
+++ BioskopWithScanner20.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class BioskopWithScanner20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[][] penonton = new String[4][2];
+
+        while (true) {
+            System.out.print("Masukkan nama : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Masukkan baris (1-4): ");
+            int baris = sc.nextInt();
+
+            System.out.print("Masukkan kolom (1-2): ");
+            int kolom = sc.nextInt();
+            sc.nextLine();
+
+            penonton[baris - 1][kolom - 1] = nama;
+
+            System.out.print("Input penonton lainnya? (iya/tidak) : ");
+            String next = sc.nextLine();
+
+            if (next.equalsIgnoreCase("tidak")) {
+                break;
+            }
+        }
+        sc.close();
+    }
+}
